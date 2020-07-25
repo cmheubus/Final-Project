9d6913dee869d513e799ea663146c6119b2a6727
Christine Marie Heubusch <christinemarieheubusch@christines-mbp.home>
2020-07-25 14:05:04 -0400

    Trying out text formatting options (stylizing and links).

--- ui.R
+++ ui.R
@@ -19,7 +19,13 @@ body <- dashboardBody(
             h2("Introduction tab content"), 
             fluidRow(
               box(plotOutput("irisPlot")),
-              box("More box content!")
+              box("More box content!"),
+              box(h3("Subtitle Here"),
+                  p(strong("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor nulla sed purus consequat, non mollis ante mattis. Maecenas vel elit finibus, tincidunt magna ac, ullamcorper purus."), 
+                    br(),
+                    a("Click here for data source", href="https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset")),
+                  p("Sed ac sagittis lorem, sit amet efficitur ante. Suspendisse massa tortor, rutrum eu pretium vitae, dapibus ac velit. Praesent at est pulvinar arcu ornare convallis feugiat nec eros. Ut eget magna sapien. Integer semper fermentum metus facilisis tempus. Proin orci elit, pulvinar non felis non, tincidunt porta metus. Vestibulum mollis dapibus urna, sed vulputate enim vestibulum eget. Duis quis diam id dui tempor vestibulum. Pellentesque sit amet dignissim velit.")
+                  )
               )
             ),
     tabItem(tabName="dataExploration",
